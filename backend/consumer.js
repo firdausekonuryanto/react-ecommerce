9fcfce64526cf65b1b8e772dffbcb69885f3137c
“akatechindonesia” <firdausekon@gmail.com>
2024-07-01 23:12:26 +0700

    rabbitMQ

---
+++ backend/consumer.js
@@ -0,0 +1,30 @@
+const amqp = require('amqplib/callback_api');
+// Connect to RabbitMQ server
+amqp.connect('amqp://localhost', (error0, connection) => {
+    if (error0) {
+        throw error0;
+    }
+
+  // Create a channel
+connection.createChannel((error1, channel) => {
+    if (error1) {
+        throw error1;
+    }
+
+const queue = 'hello';
+
+// Assert a queue into existence
+channel.assertQueue(queue, {
+    durable: false
+});
+
+console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+
+// Consume messages from the queue
+channel.consume(queue, (msg) => {
+    console.log(" [x] Received %s", msg.content.toString());
+}, {
+    noAck: true
+    });
+});
+});
---
+++ backend/publisher.js
@@ -0,0 +1,33 @@
+const amqp = require('amqplib/callback_api');
+
+// Connect to RabbitMQ server
+amqp.connect('amqp://localhost', (error0, connection) => {
+  if (error0) {
+    throw error0;
+  }
+
+  // Create a channel
+  connection.createChannel((error1, channel) => {
+    if (error1) {
+      throw error1;
+    }
+
+    const queue = 'hello';
+    const msg = 'Hello World!';
+
+    // Assert a queue into existence
+    channel.assertQueue(queue, {
+      durable: false
+    });
+
+    // Send a message to the queue
+    channel.sendToQueue(queue, Buffer.from(msg));
+    console.log(" [x] Sent %s", msg);
+  });
+
+  // Close the connection and exit
+  setTimeout(() => {
+    connection.close();
+    process.exit(0);
+  }, 500);
+});
